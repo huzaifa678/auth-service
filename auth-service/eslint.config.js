773d5dc527de3901c25a1cd85d9ead295cea09a4
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-23 17:11:17 +0500

    removed browser from lang options adding node and jest

--- auth-service/eslint.config.js
+++ auth-service/eslint.config.js
@@ -3,5 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser }, ignores: ["dist/**", "node_modules/**", "coverage/**"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.node, ...globals.jest } }, ignores: ["dist/**", "node_modules/**", "coverage/**"] },
 ]);
